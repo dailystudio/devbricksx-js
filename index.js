bf9b689a6bef776fb6ed282387489a54ae832df3
Nan YE <dailystudio2010@gmail.com>
2020-02-25 11:22:16 +0800

    [*][DEV]: add missed firebase exports;

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.logger = require('./development/logger.js');
+exports.firebase = require('./firebase/firebase.js');
 exports.firestore = require('./firebase/firestoreutils.js');
 exports.firebaseStorage = require('./firebase/firebasestorageutils.js');

2519cecdebac4c0f13b50f5d902529dab05773e6
Nan YE <dailystudio2010@gmail.com>
2020-02-25 10:45:16 +0800

    [*][DEV]: add index.js to api shortcuts;

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.development.logger = require('./development/logger.js');
+exports.firebase.firestore = require('./firebase/firestoreutils.js');
+exports.firebase.storage = require('./firebase/firebasestorageutils.js');

3ad98ee2139144e672534ef44942cfe88e914610
Nan YE <dailystudio2010@gmail.com>
2020-03-09 00:24:11 +0800

    [*][DEV]: add promise on locker.lockCall() to wait result from asyncCall;

--- async/locker.js
+++ async/locker.js
@@ -7,14 +7,20 @@ module.exports = {
 
     lockCall: function (keys, asyncCall, ...args) {
         logger.info(`locking call[${asyncCall.name}]: keys = ${JSON.stringify(keys)}, args = ${JSON.stringify(args)}`);
-        lock.acquire(keys, function(done) {
-            asyncCall(args).then(function (ret) {
-                done(null, ret);
-            });
-        }, function(err, ret) {
-            logger.info(`releasing call[${asyncCall.name}]: keys = ${JSON.stringify(keys)}, args = ${JSON.stringify(args)}`);
-            return ret;
-        }, {});
+        return new Promise((resolve, reject) => {
+            lock.acquire(keys, function(done) {
+                asyncCall(...args).then(function (ret) {
+                    done(null, ret);
+                });
+            }, function(err, ret) {
+                logger.info(`releasing call[${asyncCall.name}]: keys = ${JSON.stringify(keys)}, args = ${JSON.stringify(args)}, err = ${err}, ret = ${ret}`);
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(ret);
+            }, {});
+        });
     },
 
 };
